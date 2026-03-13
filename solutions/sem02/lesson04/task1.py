f9ae41a662f96e60b07a35f8bd9cbbdc81909c95
korolinakila <matvey.makarov07@mail.ru>
2026-03-13 23:06:17 +0300

    lesson4 dz

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,53 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+    
+    if len(image.shape) == 2:
+        length, width = image.shape[0], image.shape[1]
+        new_image = np.zeros((length + pad_size*2, width + pad_size*2), dtype=image.dtype)
+        new_image[pad_size:pad_size + length, pad_size:pad_size + width] = image
+
+    else:
+        length, width, color = image.shape[0], image.shape[1], image.shape[2]
+        new_image = np.zeros((length + pad_size*2, width + pad_size*2, color), dtype=image.dtype)
+        new_image[pad_size:pad_size + length, pad_size:pad_size + width, :] = image
+
+    return new_image
 
 
-def blur_image(
-    image: np.ndarray,
-    kernel_size: int,
-) -> np.ndarray:
-    # ваш код
-    return image
+
+
+
+
+def blur_image(image: np.ndarray, kernel_size: int,) -> np.ndarray:
+    if kernel_size < 1 or kernel_size % 2 == 0:
+        raise ValueError
+    
+    if len(image.shape) == 2:
+        pad_size = kernel_size // 2
+        new_image = pad_image(image, pad_size)
+        length, width = image.shape[0], image.shape[1]
+        result = np.zeros((length, width), dtype=np.uint8)
+
+        for i in range(length):
+            for j in range(width):
+                window = new_image[i:i + kernel_size, j:j + kernel_size]
+                result[i, j] = np.mean(window)
+                
+    else:
+        pad_size = kernel_size // 2
+        new_image = pad_image(image, pad_size)
+        length, width, color = image.shape[0], image.shape[1], image.shape[2]
+        result = np.zeros((length, width, color), dtype=np.uint8)
+
+        for i in range(length):
+            for j in range(width):
+                window = new_image[i:i + kernel_size, j:j + kernel_size, :]
+                result[i, j, :] = np.mean(window)
+
+    return result 
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,32 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    counts = np.zeros(256, dtype=np.int64)
+    for pixel in image.ravel():
+        counts[pixel] += 1
+    
+    max_pixels = 0
+    best_color = 0
+
+    for current_color in range(256):
+        if counts[current_color] > 0:
+            left = max(0, current_color - (threshold - 1))
+            right = min(255, current_color + (threshold - 1))
+            cur_sum = np.sum(counts[left:right + 1])
+
+            if cur_sum > max_pixels:
+                max_pixels = cur_sum
+                best_color = current_color
+
+            elif cur_sum == max_pixels:
+                if counts[current_color] > counts[best_color]:
+                    best_color = current_color
+    
+    total_pixels = image.size
+    percent = (max_pixels / total_pixels) * 100
+    
+    return np.uint8(best_color), float(percent)
